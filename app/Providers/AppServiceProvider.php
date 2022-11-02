26f3983c9c55fdff2bad8676f2853a681d16d11f
kodydev <100279171+kodydev@users.noreply.github.com>
2022-11-02 16:28:15 +0100

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+ 
+namespace App\Providers;
+ 
+use App\Events\ChirpCreated;
+use App\Listeners\SendChirpCreatedNotifications;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
+ 
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event to listener mappings for the application.
+     *
+     * @var array<class-string, array<int, class-string>>
+     */
+    protected $listen = [
+        ChirpCreated::class => [
+            SendChirpCreatedNotifications::class,
+        ],
+ 
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
+    ];
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
