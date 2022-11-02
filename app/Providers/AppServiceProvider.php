01cfaeda8121ae74fbbc2f91fc67ae0835ee98fb
kodydev <100279171+kodydev@users.noreply.github.com>
2022-11-02 16:37:38 +0100

    updated app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,31 +1,58 @@
 <?php
  
-namespace App\Providers;
+// namespace App\Providers;
  
-use App\Events\ChirpCreated;
-use App\Listeners\SendChirpCreatedNotifications;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
-use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+// use App\Events\ChirpCreated;
+// use App\Listeners\SendChirpCreatedNotifications;
+// use Illuminate\Auth\Events\Registered;
+// use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+// use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+// use Illuminate\Support\Facades\Event;
  
-class EventServiceProvider extends ServiceProvider
-{
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        ChirpCreated::class => [
-            SendChirpCreatedNotifications::class,
-        ],
+// class EventServiceProvider extends ServiceProvider
+// {
+//     /**
+//      * The event to listener mappings for the application.
+//      *
+//      * @var array<class-string, array<int, class-string>>
+//      */
+//     protected $listen = [
+//         ChirpCreated::class => [
+//             SendChirpCreatedNotifications::class,
+//         ],
  
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
+//         Registered::class => [
+//             SendEmailVerificationNotification::class,
+//         ],
+//     ];
 
+//     /**
+//      * Register any application services.
+//      *
+//      * @return void
+//      */
+//     public function register()
+//     {
+//         //
+//     }
+
+//     /**
+//      * Bootstrap any application services.
+//      *
+//      * @return void
+//      */
+//     public function boot()
+//     {
+//         //
+//     }
+// }
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
     /**
      * Register any application services.
      *
